a7115c76ccb1d1365d3492d172b908394f9fb024
Software-Today <nicoli.dev.430@gmail.com>
2022-09-13 05:22:07 -0700

    Add-vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
-import './bootstrap';
+require('./bootstrap');
+   
+window.Vue = require('vue');
+import VueRouter from 'vue-router'
+  
+Vue.use(VueRouter)
+   
+const routes = [
+  { path: '/', component: require('./components/Home.vue') },
+  { path: '/user', component: require('./components/User.vue') }
+]
+  
+const router = new VueRouter({
+  routes 
+})
+  
+const app = new Vue({
+  router
+}).$mount('#app')
